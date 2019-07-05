f61d0b98588c24e52f4a2854da3e2faa5778cab2
cx <cxstdio@gmail.com>
2019-07-05 10:16:26 +0800

    Create svac.h

---
+++ include/gb28181/svac.h
@@ -0,0 +1,18 @@
+/**
+ * 安全防范监控数字视音频编码
+ * SVAC: Surveillance Video and Audio Coding
+ */
+
+#ifndef _GB28181_SVAC_H_
+#define _GB28181_SVAC_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#ifdef __cplusplus
+}//extern "C"
+#endif
+
+#endif//_GB28181_SVAC_H_
