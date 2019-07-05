bb15e1833584a14c25831bf2429735fe9484300c
cx <cxstdio@gmail.com>
2019-07-05 10:25:42 +0800

    Create gb28181.h

---
+++ include/gb28181.h
@@ -0,0 +1,19 @@
+/**
+ * 公共安全视频监控联网系统信息传输、交换、控制技术要求
+ */
+
+#ifndef _GB28181_H_
+#define _GB28181_H_
+
+#include "gb28181/manscdp.h"
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#ifdef __cplusplus
+}//extern "C"
+#endif
+
+#endif//_GB28181_H_
